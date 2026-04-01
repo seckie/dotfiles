45f168e13fe75381293574b2e303b6876a10b8bd
Naoki Sekiguchi <sekiguchi@revcomm.co.jp>
2026-04-01 18:45:09 +0900

    chore(chezmoi): update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -57,6 +57,13 @@ local keymap = vim.keymap
 -- ノーマルモードでは ";" を ":" に
 keymap.set("n", ";", ":")
 
+-- メモファイルを開く
+keymap.set("n", "<Space>m", ":edit ~/Dropbox/memo.txt<CR>")
+-- init.luaを開く
+keymap.set("n", "<Space>i", ":edit ~/.local/share/chezmoi/private_dot_config/nvim/init.lua<CR>")
+-- chezmoi applyする
+keymap.set("n", "<Space>ca", ":!chezmoi apply<CR>")
+
 --
 -- Color schema
 --
