408af381ae53fddb79db487a7f0042951de7f493
Naoki Sekiguchi <sekiguchi@revcomm.co.jp>
2026-04-09 17:40:38 +0900

    chore(neovim): remove relativenumber option

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -1,7 +1,6 @@
 local opt = vim.opt
 
 opt.number = true         -- 行番号を表示
-opt.relativenumber = true -- 相対行番号を表示
 opt.tabstop = 4           -- タブ幅
 opt.shiftwidth = 4        -- インデント幅
 opt.expandtab = true      -- タブをスペースに変換　
